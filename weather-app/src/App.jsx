84d028634f6f0bee92047577dbd9610b5e58b6b9
Prabhu Teja <prabhuteja@gmail.com>
2026-01-11 12:49:03 +0530

    Project Added

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Weatherapp from './components/Weatherapp'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weatherapp />
+  </div>
+  )
+}
+
+export default App
+  
